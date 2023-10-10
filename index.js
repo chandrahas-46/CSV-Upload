47a45b07611c3b098858237ae40329f90ee904f6
Chandrahas Patel <70541620+chandrahas-46@users.noreply.github.com>
2023-10-11 02:02:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,27 +5,17 @@ import ejsLayouts from 'express-ejs-layouts';
 import csvRouter from './src/routes/csv.routes.js'
 import {connectToDB} from './src/config/mongooseConfig.js';
 
-
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(ejsLayouts);
 
 //ejs
 app.set('view engine', 'ejs');
 app.set('views',path.join(path.resolve(), 'src', 'views'));
-
-
 //routes
 app.use("/", csvRouter);
-
-
-// 3. Default request handler
-// app.get('/', (req, res) => {
-//     res.send("Welcome to CSV upload project!");
-// })
 
 //server listening
 app.listen(3000, (err) => {
